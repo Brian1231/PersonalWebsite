0cce76409c9241f58e371524666876546ac12ba1
Brian1231 <brianoleary1995@gmail.com>
2017-08-26 00:47:02 +0100

    lil bitta js

---
+++ script.js
@@ -0,0 +1,10 @@
+function ucdImg() {
+				document.getElementById("mainImage").src = "./imgs/ucd.gif";
+				$('mainImage').fadeIn(1000);
+			}
+			function projectImg() {
+				document.getElementById("mainImage").src = "./imgs/appIcon.png";
+			}
+			function fixImg() {
+				document.getElementById("mainImage").src = "./imgs/brian_cropped.jpg";
+			}
---
+++ scripts.js
@@ -0,0 +1,9 @@
+function ucdImg() {
+	document.getElementById("mainImage").src = "./imgs/ucd.gif";
+}
+function projectImg() {
+	document.getElementById("mainImage").src = "./imgs/appIcon.png";
+}
+function fixImg() {
+	document.getElementById("mainImage").src = "./imgs/brian_cropped.jpg";
+}
