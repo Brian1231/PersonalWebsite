428531bfc9a857dbea6532d00fcf5880425f31f7
Brian1231 <brianoleary1995@gmail.com>
2017-08-30 17:16:20 +0100

    email setup

--- testing.php
+++ testing.php
@@ -2,10 +2,20 @@
 <html>
 <body>
 
-<h1>My first PHP page</h1>
-
 <?php
-echo "Hello World!";
+echo "Just testing stuff";
+if(isset($_POST['email'])) {
+	echo "in here";
+}
+
+	$first_name = $_POST['name']; // required
+    $email_from = $_POST['mail']; // required
+    $comments = $_POST['message']; // required
+	
+	echo $first_name;
+	echo $email_from;
+	echo $comments;
+	
 ?> 
 
 </body>
